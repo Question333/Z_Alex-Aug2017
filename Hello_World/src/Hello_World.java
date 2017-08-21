de1033edd8700620654750f5f5a76abd59814d55
Question333 <alexzhouhappy@gmail.com>
2017-08-21 09:09:52 -0700

    Hello world completed

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -1,9 +1,13 @@
-
+/*
+ * This is the first class for Hello World and my first class in APCS2
+ * @author Alex Zhou
+ * @version version Neon 8/21/17
+ */
 public class Hello_World {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+		System.out.println("Hello World! From Alex Zhou");	
 	}
-
+	
 }
