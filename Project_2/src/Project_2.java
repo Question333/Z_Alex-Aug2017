b0247124550a0c04301ab88c0b0a945431dd1871
Question333 <alexzhouhappy@gmail.com>
2017-08-24 09:30:40 -0700

    Thrid time's a charm.

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,14 @@
+// Alex Zhou
+// 8/24/17
+// This class compares output using print and println
+
+public class Project_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+		System.out.println("Hello 2");
+		System.out.print("Hello 3");
+	}
+
+}
