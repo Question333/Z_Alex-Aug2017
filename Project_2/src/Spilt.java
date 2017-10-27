f6a8c1f1753c1259aa1de33d13381244007b0379
APCS2 <APCS2@MHS-Dreyer-34>
2017-10-27 09:51:43 -0700

    Spilt

---
+++ Project_2/src/Spilt.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+public class Spilt {
+	public static void main(String[] args) {
+		// Your task Part 0
+		//String.split();
+		//It's a method that acts on a string, <StringName>.split(<String sp>);
+		//Where sp is the string where the string splits
+		//And it returns an array
+		// Example: "I like apples!".split(" "); 
+//			it will split at spaces and return an array of ["I","like","apples!"]
+		// Example 2: "I really like really red apples".split("really") 
+//			it will split at the word "really" and return an array of ["I "," like "," red apples!"]
+		//play around with String.split! 
+		//What happens if you "I reallyreally likeapples".split("really") ?
+		System.out.println(Arrays.toString("I like apples!".split(" ")));
+		System.out.println(Arrays.toString("I really like really red apples".split("really")));
+		System.out.println(Arrays.toString("I really like reallyreally red apples".split("really")));
+		System.out.println(Arrays.toString("really like really red apples".split("really")));
+		System.out.println(Arrays.toString("I really like really red apples,really".split("really")));
+
+		//Your task Part 1:
+		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
+		* use String.split to split up the sandwich by the word "bread" and return what's in the middle of the sandwich and ignores what's on the outside
+		* What if it's a fancy sandwich with multiple pieces of bread?
+		*/
+		String string = "applespineapplesbreadlettustomatobaconmayohambreadcheese";
+		String[] parts = string.split("bread");
+		String part2 = parts[1];
+		System.out.println(part2);
+		//Your task Part 2:
+		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
+		* use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
+		* Again, what if it's a fancy sandwich with multiple pieces of bread?
+		*/
+		String string2 = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
+		
+		//"bread"
+		//"apples pineapples lettus tomato bacon mayo ham "
+		//"apples pinapples bread lettus tomate bacon mayo ham bread cheese"
+		
+	}
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner; 
+public class QuadraticClient {
+	public static void main(String[] args)
+	{
+		Scanner a1 = new Scanner(System.in);
+		boolean notdone = true;
+		while (notdone==true) {
+			System.out.println("Welcome to the Quatratic Describer");
+			System.out.println("Provide values for coefficients a, b, and c");	
+			System.out.println("a: ");
+			double a = a1.nextDouble();
+			System.out.println("b: ");
+			double b = a1.nextDouble();
+			System.out.println("c: ");
+			double c = a1.nextDouble();
+		
+			System.out.println(Quadratic.quadrDescriber(a,b,c));
+			System.out.println("Do you want to kepp going? <Type \"quit\" to end>");
+			String finished = a1.next();
+			if (finished.equals("quit"))
+				notdone = false;
+		}
+	
+	}
+}
